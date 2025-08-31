69595e36e948ceeada15ebd174153d454325a46a
Shivam-Raj <srvcode@gmail.com>
2025-08-31 16:38:38 +0530

    Adding React Component Basic Implementation

--- react-learning/03-React Essentials/3.1-concepts/3.11-components/src/App.jsx
+++ react-learning/03-React Essentials/3.1-concepts/3.11-components/src/App.jsx
@@ -1,7 +1,23 @@
+/**
+ *
+ * In React, the Component (App.jsx) is a just a JavaScript Function below 2 rules:<br>
+ *      1. Name starts with uppercase character
+ *          - The function name must start with an uppercase character to let React know that this is a Custom Component.
+ *          - Multi-word names should be written in PascalCase (e.g., MyHeader)
+ *          - It's recommended to pick a name that describes the UI building block (e.g., Header, MyHeader)
+ *
+ *      2. Returns "Renderable" content
+ *          - The function must return a value that can be rendered ("displayed on screen") by React
+ *          - In most cases: Return JSX, but also returns: string, number, boolean, null, array of allowed values.
+ *
+ */
+
+// Header is a custom component. (Starts with uppercase character)
 function Header() {
     return (
+        // header is a built-in component. (Starts with lowercase character)
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 Fundamental React concepts you will need for almost any app you are
@@ -11,15 +27,17 @@ function Header() {
     );
 }
 
+// App is a Custom Root Component. (Starts with uppercase character)
 function App() {
-    return (
-        <div>
-
-            <main>
-                <h2>Time to get started!</h2>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      {/* Self-Enclosing Syntax */}
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
 }
 
 export default App;
