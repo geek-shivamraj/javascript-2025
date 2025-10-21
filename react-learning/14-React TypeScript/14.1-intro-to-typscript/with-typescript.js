e8fbabcdebaf0c4e127108c0234c2792936dfa32
Shivam RaJ <geek.shivamraj@gmail.com>
2025-10-22 05:10:41 +0530

    Adding basic impl for typescript

---
+++ react-learning/14-React TypeScript/14.1-intro-to-typscript/with-typescript.js
@@ -0,0 +1,13 @@
+/**
+ *  - Use npm init -y to create an empty package.json to install dependencies.
+ *
+ *  - Use npx tsc with-typescript.ts command to compile this file.
+ *
+ */
+function add(a, b) {
+    return a + b;
+}
+var result1 = add(2, 5);
+console.log(result1);
+// var result2 = add('2', '5');
+// console.log(result2);
